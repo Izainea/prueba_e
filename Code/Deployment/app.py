74c669f2f455be0bcbb8126399a03cd6d79b57ea
yamitamaya <yamitamaya@gmail.com>
2023-05-11 20:48:00 -0500

    Segunda y Tercra APP

---
+++ Code/Deployment/app.py
@@ -0,0 +1,41 @@
+## Tercer APP Dash
+from dash import Dash, html, dcc, callback, Output, Input
+import plotly.express as px
+import pandas as pd
+
+df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminderDataFiveYear.csv')
+
+app = Dash(__name__)
+
+app.layout = html.Div([
+    html.H1(children='Mi tercer APP',style={'textAlign':'center'}),
+    html.P('texto de la tercera APP'),
+    dcc.Graph(id='graph-with-slider'),
+    dcc.Slider(
+        df['year'].min(),
+        df['year'].max(),
+        step=None,
+        value=df['year'].min(),
+        marks={str(year): str(year) for year in df['year'].unique()},
+        id='year-slider'
+    )
+])
+
+
+@app.callback(
+    Output('graph-with-slider', 'figure'),
+    Input('year-slider', 'value'))
+def update_figure(selected_year):
+    filtered_df = df[df.year == selected_year]
+
+    fig = px.scatter(filtered_df, x="gdpPercap", y="lifeExp",
+                     size="pop", color="continent", hover_name="country",
+                     log_x=True, size_max=55)
+
+    fig.update_layout(transition_duration=500)
+
+    return fig
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True, port=8051)
